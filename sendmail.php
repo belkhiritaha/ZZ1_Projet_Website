299761791ace55b84fae6d906e9da7ed43f2dbf7
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-06-19 12:18:09 +0200

    added ZZ1_Project folder

--- sendmail.php
+++ sendmail.php
@@ -1,14 +1,10 @@
 <?php
-    $to      = 'belkhiri.taha53@gmail.com';
+    $to      = "belkhiri.taha53@gmail.com";
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = array(
-        'From' => $_POST["from"],
-        'Reply-To' => $_POST["from"],
-        'X-Mailer' => 'PHP/' . phpversion()
-    );
 
-    if (mail($to, $subject, $message, $headers)){
+
+    if (mail($to, $subject, $message)){
         echo "Mail sent successfully";
     }
     else{
