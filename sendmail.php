ccfc7b3174c14807fd1bb5c2f25b3b75843473ec
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-07-06 14:11:30 +0200

    update auto mailing

--- sendmail.php
+++ sendmail.php
@@ -2,9 +2,10 @@
     $to      = "belkhiri.taha53@gmail.com";
     $subject = $_POST["subject"];
     $message = $_POST["message"];
+    $header = "From: " . $_POST["from"];
 
 
-    if (mail($to, $subject, $message)){
+    if (mail($to, $subject, $message, $header)) {
         echo "Mail sent successfully";
     }
     else{
